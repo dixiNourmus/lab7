9961f9b989b66587683f307dc301d0d6f4d3c694
dixiNourmus <joshua.berger@talawanda.org>
2024-03-06 13:36:43 -0500

    i

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        for(int i =0; i <= 30; i++){
+            System.out.println(i);
+        }
+        for(int i =30; i <= 0; i--){
+            System.out.println(i);
+        }
+        for(int i =0; i <= 18; i = i +3){
+            System.out.println(i);
+        }
+        for(int i =10; i <= 0; i= i-2){
+            System.out.println(i);
+        }
+
+
+
+
+
+
+
+    }
+}
