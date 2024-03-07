f799035fbfa3478b962fd01ca176a39d902ebc90
dixiNourmus <joshua.berger@talawanda.org>
2024-03-07 13:50:21 -0500

    i

---
+++ src/lettre.java
@@ -0,0 +1,51 @@
+public class lettre {
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i <= 1; i ++){
+
+            for (int j = 0; j <= 11; j ++){
+
+                System.out.print("*");
+
+
+            }
+            System.out.println();
+        }
+        for (int i = 0; i <= 6; i ++){
+
+
+            for (int j = 0; j <= 3; j ++){
+
+                System.out.print(" ");
+
+
+            }
+            for (int j = 0; j <= 3; j ++){
+
+                System.out.print("*");
+
+
+            }
+            System.out.println();
+
+        }
+        for (int i = 0; i <= 1; i ++){
+
+            for (int j = 0; j <= 7; j ++){
+
+                System.out.print("*");
+
+
+            }
+            System.out.println();
+        }
+
+
+    }
+
+
+
+
+
+}
